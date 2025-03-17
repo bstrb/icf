60fb7c4f8d1ed075f9779aaecd8820c0ca474e6f
Buster Blomberg <buster.blomberg@gmail.com>
2025-03-17 21:36:43 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   icf/__pycache__/ICFTOTAL.cpython-310.pyc
    	modified:   icf/__pycache__/image_processing.cpython-310.pyc
    	modified:   icf/icf_ui_find_centers.ipynb
    	modified:   icf/image_processing.py

--- icf/image_processing.py
+++ icf/image_processing.py
@@ -1,6 +1,31 @@
 # image_processing.py
+import numpy as np
+import h5py
 
 from ICFTOTAL import center_of_mass_initial_guess, find_diffraction_center
+
+def compute_center_for_frame(args):
+    """
+    Helper function for multiprocessing.
+    Loads one frame from the H5, calls process_single_image, returns (frame_num, center_x, center_y).
+    If the center is invalid or out-of-bounds, it returns NaN.
+    """
+    (frame_num, image_file, mask, n_wedges, n_rad_bins,
+     xatol, fatol, verbose, xmin, xmax, ymin, ymax) = args
+
+    # Load the image for this frame
+    with h5py.File(image_file, 'r') as f:
+        img = f['/entry/data/images'][frame_num].astype(np.float32)
+
+    # Run the center-finding function
+    cx, cy = process_single_image(img, mask, n_wedges, n_rad_bins, xatol, fatol, verbose)
+
+    # If out-of-bounds or invalid => NaN
+    if not (np.isfinite(cx) and np.isfinite(cy) and
+            xmin <= cx < xmax and ymin <= cy < ymax):
+        cx, cy = np.nan, np.nan
+
+    return frame_num, cx, cy
 
 def process_single_image(img, mask, n_wedges, n_rad_bins, xatol, fatol, verbose):
     """
